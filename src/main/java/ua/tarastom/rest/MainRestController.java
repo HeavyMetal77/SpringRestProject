c5c981f85e8edd3e676b7dedc00e0f3132500c75
HeavyMetal77 <ttm2006@ukr.net>
2020-01-12 18:59:33 +0200

    start SpringRestProject

---
+++ src/main/java/ua/tarastom/rest/MainRestController.java
@@ -0,0 +1,15 @@
+package ua.tarastom.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class MainRestController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
