43ef2898e3a6ef4dfcb18b6353cc870cf2a21e4e
HeavyMetal77 <ttm2006@ukr.net>
2020-01-14 21:55:52 +0200

    Using @PathVariable for REST Endpoints

--- src/main/java/ua/tarastom/rest/StudentRestController.java
+++ src/main/java/ua/tarastom/rest/StudentRestController.java
@@ -1,10 +1,12 @@
 package ua.tarastom.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.tarastom.entity.Student;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void LoadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Mary", "Black"));
+        theStudents.add(new Student("Jack", "Dude"));
+        theStudents.add(new Student("Susan", "Oliver"));
+    }
+
+
     @GetMapping("/students")
     public List<Student> getListStudents() {
-        ArrayList<Student> students = new ArrayList<Student>();
-
-        students.add(new Student("Mary", "Black"));
-        students.add(new Student("Jack", "Dude"));
-        students.add(new Student("Susan", "Oliver"));
-
-        return students;
+        return theStudents;
     }
+
+    @GetMapping("/students/{studentId}")
+    public Student getTheStudent(@PathVariable("studentId") int studentId) {
+        return theStudents.get(studentId);
+    }
+
 }
