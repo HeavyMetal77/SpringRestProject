8b9ac3beec849b2ce414af758beae6d9782dca7e
HeavyMetal77 <ttm2006@ukr.net>
2020-01-15 14:05:13 +0200

    добавлены методы обработки ошибок при вводе направильных значений

--- src/main/java/ua/tarastom/rest/StudentRestController.java
+++ src/main/java/ua/tarastom/rest/StudentRestController.java
@@ -1,9 +1,8 @@
 package ua.tarastom.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ua.tarastom.entity.Student;
 
 import javax.annotation.PostConstruct;
@@ -32,7 +31,32 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getTheStudent(@PathVariable("studentId") int studentId) {
+        if (studentId < 0 || studentId >= theStudents.size()) {
+            throw new StudentNotFoundException("Student id is not found " + studentId);
+        }
         return theStudents.get(studentId);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException studentNFE) {
+        StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+
+        studentErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        studentErrorResponse.setMessage(studentNFE.getMessage());
+        studentErrorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(studentErrorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    //для любых других исключений - ошибка 400
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception theAnyException) {
+        StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
+
+        studentErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        studentErrorResponse.setMessage(theAnyException.getMessage());
+        studentErrorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(studentErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
